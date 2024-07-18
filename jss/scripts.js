1c7d101f825f5932517f7e36d3f0b376bc7d12bd
anisadinda13 <anisadinda13@gmail.com>
2024-07-18 20:47:56 +0700

    config mini prject

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,1 +1,56 @@
-//Ini Javascript
+console.log('external Js')
+const Weight = document.getElementById('input-Weight');
+const Age = document.getElementById('input-Age');
+const Heigh = document.getElementById('input-Heigh');
+const resultBMIElement = document.querySelector('.result-BMI');
+const categoryBMIElement = document.querySelector('.category-BMI');
+const dataUserElement = document.querySelector('data-user .Age');
+const genderElement = document.querySelector('data-user .Gender');
+
+//Klasfikasi hasil BMI
+function klasfikasiBMI(bmi){
+    if (bmi<18.5){
+        return "Underweight";
+    }else if (bmi < 25){
+        return "Normal (Ideal)";
+    }else if (bmi < 30){
+        return "Overweight";
+    } else {
+        return "Obesity";
+    }
+}
+
+//Perhitungan dan menampilkan hasil pada html page 
+function hitungBmi(event){
+    event.preventDefault(); 
+
+    const Hg = parseFloat(Heigh.value)/ 100;
+    const Wg = parseFloat(Weight.value);
+    const Ag = parseFloat(Age.value, 10);
+
+    if (isNaN(Weight) || isNaN(Height) || Weight <= 0 || Height  <=0 {
+        alert ("Please enter valid values for weight and ")
+        return;
+    }
+
+    let bmi = Wg / (Hg * Hg);
+    bmi = bmi.toFixed(1);
+
+    resultBMIElement.textContent = bmi;
+    categoryBMIElement.textContent = '${klasifikasi(parseFloat(bmi))}';
+    dataUserElement.textContent ='$(Ag)';
+    genderElement.textContent = gender.value;
+}
+
+//Reset Button 
+function resetForm() {
+    document.getElementById('bmi-form').reset();
+    resultBMIElement.textContent = '0.0';
+    categoryBMIElement.textContent = '';
+    dataUserElement.textContent = '';
+    genderElement.textContent = '';
+}
+
+//Event Submit dan Reset
+document.getElementById('form-submit').addEventListener('submit', Submitbmi);
+document.getElementById('reset-button').addEventListener('click',resetForm);
